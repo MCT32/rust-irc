e5216f700de36459acb6b7e8200e0e3f5b04b610
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-02-11 17:28:30 +1100

    Added IRC initiation and quit function

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,47 @@
 mod messages;
 
 
-use messages::Message;
-use tokio::{io, net::TcpStream, sync::Mutex};
-use std::{io::Error, net::{IpAddr, Ipv4Addr, SocketAddr}, str::FromStr, sync::Arc};
+use messages::{Message, Params};
+use tokio::{io::{self, Interest}, net::TcpStream, sync::Mutex};
+use core::fmt;
+use std::{error::Error, net::{IpAddr, Ipv4Addr, SocketAddr}, str::FromStr, sync::Arc};
 
 
 type RawMessageHandler = fn(&str);
 type MessageHandler = fn(messages::Message);
 
+// TODO: Improve error verbosity
+#[derive(Debug)]
+pub struct IrcConnectError;
+impl Error for IrcConnectError {}
+impl fmt::Display for IrcConnectError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "could not connect to irc server")
+    }
+}
+
+// TODO: Improve error verbosity
+#[derive(Debug)]
+pub struct IrcSendError;
+impl Error for IrcSendError {}
+impl fmt::Display for IrcSendError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "could not send message")
+    }
+}
+
 #[derive(Clone)]
 pub struct IrcConfig {
     host: SocketAddr,
+
+    nickname: String,
+    username: String,
+    hostname: String,
+    servername: String,
+    realname: String,
+
+    password: Option<String>,
+
     raw_receive_handler: Option<RawMessageHandler>,
     receive_handler: Option<MessageHandler>,
 }
@@ -20,6 +50,15 @@ impl IrcConfig {
     pub fn new() -> Self {
         IrcConfig {
             host: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 6667),
+
+            nickname: "".to_string(),
+            username: "".to_string(),
+            hostname: "".to_string(),
+            servername: "".to_string(),
+            realname: "".to_string(),
+
+            password: None,
+
             raw_receive_handler: None,
             receive_handler: None,
         }
@@ -40,19 +79,31 @@ impl IrcConfig {
         self
     }
 
-    pub async fn connect(&self) -> Result<IrcConnection, Error> {
+    fn check_info(&self) -> bool {
+        !self.nickname.is_empty()
+            && !self.username.is_empty()
+            && !self.hostname.is_empty()
+            && !self.servername.is_empty()
+            && !self.realname.is_empty()
+    }
+
+    pub async fn connect(&self) -> Result<IrcConnection, IrcConnectError> {
+        if !self.check_info() {
+            return Err(IrcConnectError);
+        }
+
         match TcpStream::connect(self.host).await {
             Ok(stream) => {
-                let connection = IrcConnection {
+                let mut connection = IrcConnection {
                     stream: Arc::new(Mutex::new(stream)),
                     config: self.clone(),
                 };
 
-                connection.init();
+                connection.init().await;
 
                 Ok(connection)
             }
-            Err(err) => Err(err)
+            Err(_) => Err(IrcConnectError)
         }
     }
 }
@@ -64,23 +115,59 @@ pub struct IrcConnection {
 }
 
 impl IrcConnection {
-    pub async fn send_raw<T: Into<String>>(&mut self, msg: T) -> Result<usize, Error> {
+    pub async fn send_raw<T: Into<String>>(&mut self, msg: T) -> Result<usize, IrcSendError> {
         let mut msg: String = msg.into();
         msg.push_str("\n");
-        self.stream.lock().await.try_write(msg.as_bytes())
+        match self.stream.lock().await.try_write(msg.as_bytes()) {
+            Ok(bytes_sent) => Ok(bytes_sent),
+            Err(_) => Err(IrcSendError)
+        }
     }
 
-    pub async fn send(&mut self, msg: Message) -> Result<usize, Error> {
+    pub async fn send(&mut self, msg: Message) -> Result<usize, IrcSendError> {
         let mut msg = msg.to_string();
         msg.push_str("\n");
         print!("{}", msg);
-        self.stream.lock().await.try_write(msg.as_bytes())
+        match self.stream.lock().await.try_write(msg.as_bytes()) {
+            Ok(bytes_sent) => Ok(bytes_sent),
+            Err(_) => Err(IrcSendError)
+        }
     }
 
-    pub fn init(&self) {
+    pub async fn init(&mut self) {
         if self.config.raw_receive_handler.is_some() || self.config.receive_handler.is_some() {
             tokio::spawn(Self::receive_loop(self.config.clone(), self.stream.clone()));
         }
+
+        self.stream.lock().await.ready(Interest::READABLE | Interest::WRITABLE).await.unwrap();
+
+        if let Some(password) = &self.config.password {
+            self.send(Message {
+                prefix: None,
+                command: "PASS".to_string(),
+                params: Params(vec![password.to_string()])
+            }).await.unwrap();
+        }
+
+        self.send(Message {
+            prefix: None,
+            command: "NICK".to_string(),
+            params: Params(vec![self.config.nickname.clone()])
+        }).await.unwrap();
+
+        self.send(Message {
+            prefix: None,
+            command: "USER".to_string(),
+            params: Params(vec![self.config.username.clone(), self.config.hostname.clone(), self.config.servername.clone(), self.config.realname.clone()])
+        }).await.unwrap();
+    }
+
+    pub async fn quit(&mut self) {
+        self.send(Message {
+            prefix: None,
+            command: "QUIT".to_string(),
+            params: Params(vec![])
+        }).await.unwrap();
     }
 
     async fn receive_loop(config: IrcConfig, stream: Arc<Mutex<TcpStream>>) {
