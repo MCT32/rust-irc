ed93fac7c7303697f7b58faee1d9993a5f99ec88
toastinthetub <johnapplebiz@gmail.com>
2024-06-16 22:23:20 -0700

    fixed modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
-mod irc_enums;
 mod config;
 mod error;
+mod irc_enums;
 
-use tokio::sync::mpsc::{Sender, Receiver};
-use irc_enums::{IrcCommand, IrcEvent};
 use config::IrcConfig;
+use irc_enums::{IrcCommand, IrcEvent};
+use tokio::sync::mpsc::{Receiver, Sender};
 
-pub async fn backend(config: IrcConfig<'_>, tx: Sender<IrcEvent<'_>>, rx: Receiver<IrcCommand<'_>>) {
-    let stream = tokio::net::TcpStream::connect(config.server_address).await.unwrap();
+pub async fn backend(
+    config: IrcConfig<'_>,
+    tx: Sender<IrcEvent<'_>>,
+    rx: Receiver<IrcCommand<'_>>,
+) {
+    let stream = tokio::net::TcpStream::connect(config.server_address)
+        .await
+        .unwrap();
 }
