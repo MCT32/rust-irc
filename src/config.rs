e8c4bbbf0b117e402d04c85b8aee7cfebbda2574
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-06-17 12:01:31 +1000

    added user info to config

--- src/config.rs
+++ src/config.rs
@@ -2,29 +2,53 @@ use std::net::{SocketAddr, ToSocketAddrs};
 
 use crate::error::IrcConfigBuilderError;
 
-pub struct IrcConfig {
-    server_address: SocketAddr,
+pub struct IrcConfig<'a> {
+    pub server_address: SocketAddr,
+    pub username: &'a str,
+    pub nickname: &'a str,
+    pub password: Option<&'a str>,
 }
 
-pub struct IrcConfigBuilder {
+pub struct IrcConfigBuilder<'a> {
     server_address: Option<SocketAddr>,
+    username: Option<&'a str>,
+    nickname: Option<&'a str>,
+    password: Option<&'a str>,
 }
 
-impl IrcConfigBuilder {
+impl<'a> IrcConfigBuilder<'a> {
     pub fn new() -> Self {
         IrcConfigBuilder {
             server_address: None,
+            username: None,
+            nickname: None,
+            password: None,
         }
     }
 
-    pub fn build(self) -> Result<IrcConfig, IrcConfigBuilderError> {
+    pub fn build(self) -> Result<IrcConfig<'a>, IrcConfigBuilderError> {
         let server_address = match self.server_address {
             Some(server_address) => server_address,
             None => return Err(IrcConfigBuilderError::ServerAddressMissing),
         };
 
+        let username = match self.username {
+            Some(username) => username,
+            None => return Err(IrcConfigBuilderError::UsernameMissing),
+        };
+
+        let nickname = match self.nickname {
+            Some(nickname) => nickname,
+            None => username,
+        };
+
+        let password = self.password;
+
         Ok(IrcConfig {
             server_address,
+            username,
+            nickname,
+            password,
         })
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ use tokio::sync::mpsc::{Sender, Receiver};
 use irc_enums::{IrcCommand, IrcEvent};
 use config::IrcConfig;
 
-pub fn backend(config: IrcConfig, tx: Sender<IrcEvent>, rx: Receiver<IrcCommand>) {
-
+pub async fn backend(config: IrcConfig<'_>, tx: Sender<IrcEvent<'_>>, rx: Receiver<IrcCommand<'_>>) {
+    let stream = tokio::net::TcpStream::connect(config.server_address).await.unwrap();
 }
