8cf8fd4b9503e8af973380c4367fdba1565a40f1
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-06-18 21:48:41 +1000

    tests, yay!

--- src/message.rs
+++ src/message.rs
@@ -15,19 +15,19 @@ impl TryFrom<&str> for GenericIrcMessage {
 
     fn try_from(value: &str) -> Result<GenericIrcMessage, Error> {
         // TODO: remake regex to allow all possible params, with symbols and such
-        let re = Regex::new("^(?::([A-Za-z0-9]+) )?([A-Z]+|[0-9]{3})(?: ([A-Za-z0-9: ]+))?$").unwrap();
+        let re = Regex::new("^(?::([A-Za-z0-9]+) )?([A-Z]+|[0-9]{3})(?: ([A-Za-z0-9:#\\/! ]+))?$").unwrap();
 
         let Some(caps) = re.captures(value) else {
             return Err(Error::NoMatch(value.to_string()));
         };
 
-        let prefix = caps.get(0).map(|m| m.as_str().to_string());
+        let prefix = caps.get(1).map(|m| m.as_str().to_string());
 
-        let Some(command) = caps.get(1).map(|m| m.as_str().to_string()) else {
+        let Some(command) = caps.get(2).map(|m| m.as_str().to_string()) else {
             return Err(Error::NoCommand(value.to_string()));
         };
 
-        let params = match caps.get(2).map(|m| m.as_str()) {
+        let params = match caps.get(3).map(|m| m.as_str()) {
             None => vec![],
             Some(params) => {
                 match params.split_once(" :") {
@@ -69,5 +69,52 @@ impl From<GenericIrcMessage> for String {
         };
 
         buffer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_string() {
+        assert_eq!("LEAVE".try_into(), Ok(GenericIrcMessage {
+            prefix: None,
+            command: "LEAVE".to_string(),
+            params: vec![],
+        }));
+
+        assert_eq!(":server MSG #meme :11/10 cock".try_into(), Ok(GenericIrcMessage {
+            prefix: Some("server".to_string()),
+            command: "MSG".to_string(),
+            params: vec!["#meme", "11/10 cock"].into_iter().map(|m| m.to_string()).collect(),
+        }));
+
+        assert_eq!(":server 404 shit".try_into(), Ok(GenericIrcMessage {
+            prefix: Some("server".to_string()),
+            command: "404".to_string(),
+            params: vec!["shit".to_string()],
+        }));
+    }
+
+    #[test]
+    fn to_string() {
+        assert_eq!("LEAVE".to_string(), String::from(GenericIrcMessage {
+            prefix: None,
+            command: "LEAVE".to_string(),
+            params: vec![],
+        }));
+
+        assert_eq!(":server MSG #meme 11/10cock".to_string(), String::from(GenericIrcMessage {
+            prefix: Some("server".to_string()),
+            command: "MSG".to_string(),
+            params: vec!["#meme", "11/10cock"].into_iter().map(|m| m.to_string()).collect(),
+        }));
+
+        assert_eq!(":server 404 shit".to_string(), String::from(GenericIrcMessage {
+            prefix: Some("server".to_string()),
+            command: "404".to_string(),
+            params: vec!["shit".to_string()],
+        }));
     }
 }
